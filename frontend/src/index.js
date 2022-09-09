454688a3d7f6ab4fd77e14e7a675d2ca91c0e132
mrslvs <sekerka.miroslav@outlook.com>
2022-09-09 19:41:59 +0200

    separate styles

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./css/index.css";
 import App from "./App";
+import Home from "./Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  //   <React.StrictMode>
-  <App />
-  //   </React.StrictMode>
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="*" element={<Home />} />
+        <Route path="/app" element={<App />} />
+        {/* <PrivateRoute path="/app" element={<App />} /> */}
+      </Routes>
+    </Router>
+  </React.StrictMode>
 );
