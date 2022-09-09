32801884889a6cf3010aa6e1bce5432b48b5bb06
mrslvs <sekerka.miroslav@outlook.com>
2022-09-09 19:59:30 +0200

    add background to homepage

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,6 +1,9 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
+import "./css/Home.css";
 
 const Home = () => {
+  const [selection, setSelection] = useState();
+
   return (
     <main>
       <h1>Home</h1>
