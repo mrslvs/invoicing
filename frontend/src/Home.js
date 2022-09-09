7346882b023bdac8f27a7bc53dbda10ef0a9ec38
mrslvs <sekerka.miroslav@outlook.com>
2022-09-09 21:08:36 +0200

    add buttons & styling

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -2,11 +2,21 @@ import { React, useState, useEffect } from "react";
 import "./css/Home.css";
 
 const Home = () => {
-  const [selection, setSelection] = useState();
+  const [selection, setSelection] = useState("about");
 
   return (
     <main>
-      <h1>Home</h1>
+      <div className="contentBox">
+        <button onClick={(e) => setSelection(e.target.value)} value="about">
+          About
+        </button>
+        <button onClick={(e) => setSelection(e.target.value)} value="login">
+          Login
+        </button>
+        <button onClick={(e) => setSelection(e.target.value)} value="register">
+          Register
+        </button>
+      </div>
     </main>
   );
 };
