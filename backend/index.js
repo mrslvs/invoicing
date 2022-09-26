fd3c8093fecf12fcba73487a8d26c93e0aa9eaa4
mrslvs <sekerka.miroslav@outlook.com>
2022-09-26 14:27:50 +0200

    add CORS to allow frontend access backend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const PORT = process.env.PORT || 9000;
+
+const whitelist = ['http://localhost:3000'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 app.get('^/$|/index(.html)?', (req, res) => {
     // ^/ -> must start with /
