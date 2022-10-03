5c11dee4f7b9bd0ad1213efca05fc4a9b4dd1e14
mrslvs <sekerka.miroslav@outlook.com>
2022-10-03 21:52:08 +0200

    refactor register page & add email input

--- frontend/src/home/Register.js
+++ frontend/src/home/Register.js
@@ -6,6 +6,10 @@ const Register = () => {
     // https://youtu.be/brcHK3P6ChQ
     //must start lower or upercase letter, then char or number or - or _
     const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+
+    // https://www.simplilearn.com/tutorials/javascript-tutorial/email-validation-in-javascript
+    const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
     // Min 8 chars, 1 uppercase letter, 1 lowercase letter, 1 number, 1 special character:
     // (https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a)
     const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
@@ -13,11 +17,16 @@ const Register = () => {
     const [user, setUser] = useState('');
     const [validUser, setValidUser] = useState(false);
 
+    const [mail, setMail] = useState('');
+    const [validMail, setValidMail] = useState(false);
+
     const [pwd, setPwd] = useState('');
     const [validPwd, setValidPwd] = useState(false);
 
     const [repeatPwd, setRepeatPwd] = useState('');
     const [validRepeatPwd, setValidRepeatPwd] = useState(false);
+
+    const [validForm, setValidForm] = useState(false);
 
     useEffect(() => {
         const result = USER_REGEX.test(user);
@@ -25,10 +34,29 @@ const Register = () => {
     }, [user]);
 
     useEffect(() => {
+        const result = EMAIL_REGEX.test(mail);
+        setValidMail(result);
+    }, [mail]);
+
+    useEffect(() => {
         const result = PWD_REGEX.test(pwd);
         console.log(pwd + ' = ' + result);
         setValidPwd(result);
     }, [pwd]);
+
+    useEffect(() => {
+        if (pwd && repeatPwd && repeatPwd === pwd) {
+            setValidRepeatPwd(true);
+            console.log('passwords match');
+        } else {
+            setValidRepeatPwd(false);
+        }
+    }, [repeatPwd]);
+
+    useEffect(() => {
+        const tmp = validUser && validMail && validPwd && validRepeatPwd;
+        setValidForm(tmp);
+    }, [validUser, validMail, validPwd, validRepeatPwd]);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -51,13 +79,7 @@ const Register = () => {
     };
 
     return (
-        <form
-            // id="register-form"
-            action="/auth/register"
-            method="POST"
-            className="Content register-form"
-            onSubmit={(e) => handleSubmit(e)}
-        >
+        <form className="Content register-form" onSubmit={(e) => handleSubmit(e)}>
             <label htmlFor="username">Username</label>
 
             <input
@@ -73,9 +95,20 @@ const Register = () => {
                 <p>Valid</p>
             ) : (
                 <p className="hint">
-                    Username must contain 3-23 characters/numbers and start with letter
+                    Username must contain 3-23 characters/numbers and start with a letter
                 </p>
             )}
+
+            <label htmlFor="usermail">Email</label>
+
+            <input
+                type="text"
+                id="usermail"
+                name="usermail"
+                autoComplete="off"
+                required
+                onChange={(e) => setMail(e.target.value)}
+            />
 
             <label htmlFor="pwd">Password</label>
 
@@ -98,10 +131,10 @@ const Register = () => {
                 required
                 readOnly={validPwd ? false : true}
                 placeholder={validPwd ? '' : 'Enter valid password first'}
-                onChange={(e) => setValidRepeatPwd(e.target.value)}
+                onChange={(e) => setRepeatPwd(e.target.value)}
             />
 
-            <input type="submit" />
+            <input type="submit" disabled={!validForm} />
         </form>
     );
 };
