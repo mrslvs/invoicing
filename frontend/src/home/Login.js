93ba76bfdb4bd34b2d5a79454f2819e6b53b4201
mrslvs <sekerka.miroslav@outlook.com>
2022-10-05 19:56:25 +0200

    add handleSubmit function for login

--- frontend/src/home/Login.js
+++ frontend/src/home/Login.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import axios from 'axios';
 
 const Login = () => {
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const api_endpoint = 'http://localhost:9000/auth/login';
+
+        // send POST request
+        const body = {};
+        const formData = new FormData(document.querySelector('.login-form'));
+
+        formData.forEach((val, key) => {
+            body[key] = val;
+            console.log(key + '=' + val);
+        });
+
+        console.log(body);
+        const res = await axios.post(api_endpoint, body);
+
+        console.log(res);
+    };
     return (
-        <form action="/auth/login" method="POST" className="Content">
+        <form className="Content login-form" onSubmit={(e) => handleSubmit(e)}>
             <label htmlFor="username">Username</label>
-            <input type="text" />
+            <input type="text" name="username" id="username" />
             <label htmlFor="pwd">Password</label>
-            <input type="password" />
+            <input type="password" name="pwd" id="pwd" />
+            <input type="submit" />
         </form>
     );
 };
