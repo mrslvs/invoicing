1b312387075b65c9b3e1a27943955c7d3e0f63d6
mrslvs <sekerka.miroslav@outlook.com>
2022-10-07 01:50:38 +0200

    add fixed width&height to content box

--- frontend/src/home/About.js
+++ frontend/src/home/About.js
@@ -3,7 +3,20 @@ import React from 'react';
 const About = () => {
     return (
         <section className="Content">
-            <h1>About</h1>
+            <h1>NamenameName</h1>
+            <p>
+                Welcome personal project NamenameName, a fullstack application build with modern
+                technologies (MySQL, ExpressJS, ReactJS + NodeJS).Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+                laborum.
+            </p>
+            <p>
+                Made by <a href="#">mrslvs</a>
+            </p>
         </section>
     );
 };
