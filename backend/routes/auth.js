bf4006d0973f6a8458a30985276635128d4ebd24
mrslvs <sekerka.miroslav@outlook.com>
2022-10-06 12:58:12 +0200

    add handle refresh token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser } = require('../controllers/authController');
+const handleRefreshToken = require('../controllers/refreshTokenController');
 
 router.post('/register', (req, res) => registerUser(req, res));
 
 router.post('/login', (req, res) => loginUser(req, res));
+
+router.get('/refresh', (req, res) => handleRefreshToken(req, res));
 
 module.exports = router;
