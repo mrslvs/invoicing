645426f5f315c304b639f2686a2789c0c324648c
mrslvs <sekerka.miroslav@outlook.com>
2022-10-10 12:03:16 +0200

    add authContext

--- frontend/src/home/Login.js
+++ frontend/src/home/Login.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import axios from '../api/axios';
+import AuthContext from '../context/AuthProvider';
 
 const Login = () => {
+    const { setAuth } = useContext(AuthContext);
+
     const handleSubmit = async (e) => {
         e.preventDefault();
 
@@ -14,29 +17,40 @@ const Login = () => {
             console.log(key + '=' + val);
         });
 
+        console.log(body);
+
         try {
             const res = await axios.post('/auth/login', body);
+
+            const accessToken = res.data.accessToken;
+
+            setAuth({ accessToken });
         } catch (err) {
             console.log('mame err');
             console.log(err.code);
             if (err.code === 'ERR_NETWORK') {
-                console.log('no server response');
+                console.log('ERROR: no server response');
+            } else if (err.response.status === 400) {
+                console.log('ERROR: missing username or password');
+            } else {
+                console.log('ERROR: login failed');
             }
         }
 
         // console.log(res);
     };
+
     return (
         <form className="Content login-form" onSubmit={(e) => handleSubmit(e)}>
             <div className="form-item">
                 <label htmlFor="username">Username</label>
-                <input type="text" name="username" id="username" />
+                <input type="text" name="username" id="username" autoComplete="off" />
             </div>
             <div className="marbot"></div>
 
             <div className="form-item">
                 <label htmlFor="pwd">Password</label>
-                <input type="password" name="pwd" id="pwd" />
+                <input type="password" name="pwd" id="pwd" autoComplete="off" />
             </div>
             <div className="marbot"></div>
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./css/index.css";
-import App from "./App";
-import Home from "./Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './css/index.css';
+import App from './App';
+import Home from './Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthProvider';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="*" element={<Home />} />
-        <Route path="/app" element={<App />} />
-        {/* <PrivateRoute path="/app" element={<App />} /> */}
-      </Routes>
-    </Router>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Router>
+            <Routes>
+                <Route path="*" element={<Home />} />
+                {/* <PrivateRoute path="/app" element={<App />} /> */}
+                {/* <AuthProvider> */}
+                <Route path="/app" element={<App />} />
+                {/* </AuthProvider> */}
+            </Routes>
+        </Router>
+    </React.StrictMode>
 );
