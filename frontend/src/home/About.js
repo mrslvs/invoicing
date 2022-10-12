a8f1acf22d35d82c099294176a6a2fb7ceaf8bb6
mrslvs <sekerka.miroslav@outlook.com>
2022-10-12 19:43:01 +0200

    add description to about page

--- frontend/src/home/About.js
+++ frontend/src/home/About.js
@@ -6,13 +6,8 @@ const About = () => {
             <h1>NamenameName</h1>
             <p>
                 Welcome personal project NamenameName, a fullstack application build with modern
-                technologies (MySQL, ExpressJS, ReactJS + NodeJS).Lorem ipsum dolor sit amet,
-                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-                laborum.
+                technologies (MySQL, ExpressJS, ReactJS + NodeJS). This application allows user to
+                view invoices and perform CRUD operations upon them.
             </p>
             <p>
                 Made by <a href="#">mrslvs</a>
