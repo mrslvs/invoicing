32b368be8ba74cc16dd7d4919c5da4d4fd938db3
mrslvs <sekerka.miroslav@outlook.com>
2024-03-18 20:15:19 +0100

    change AuthProvider

--- frontend/src/context/AuthProvider.js
+++ frontend/src/context/AuthProvider.js
@@ -1,11 +1,14 @@
-import { createContext, useState } from 'react';
+import { createContext, useState } from 'react'
 
-const AuthContext = createContext({});
+const AuthContext = createContext()
 
 export const AuthProvider = ({ children }) => {
-    const [auth, setAuth] = useState({});
+    const [user, setUser] = useState({
+        user: '',
+        isLoggedIn: false,
+    })
 
-    return <AuthContext.Provider value={{ auth, setAuth }}>{children}</AuthContext.Provider>;
-};
+    return <AuthContext.Provider value={{ user, setUser }}>{children}</AuthContext.Provider>
+}
 
-export default AuthContext;
+export default AuthContext
